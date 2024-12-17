dbef860885384550115aa77af5eed42a28b5829a
Rahulsinha1608 <23052015@kiit.ac.in>
2024-12-17 21:15:46 +0530

    Another comit from owner ;)

---
+++ LaunchApp1.java
@@ -0,0 +1,12 @@
+class Launch{
+	public static void main(String[] args){
+			System.out.println("Exploring Git & GitHub.");
+	}
+public static void fun(){
+	int a=10;
+int b=29;
+int sum=a+b;
+System.out.println(sum);
+
+}
+}
